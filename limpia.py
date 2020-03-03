e39dee97ae217998eb5158eb099a65ea90389c2d
i4vk <ivangs98@gmail.com>
2020-03-03 17:44:39 +0100

    Modificado limpia script

--- limpia.py
+++ limpia.py
@@ -1,21 +1,20 @@
 import os
 import shutil
+import eval.conversion as conv
 
-path = "dataset/train_formulas/"
-archivos = os.listdir(path)
-
+source_path = "dataset/train_formulas/"
 num_claus = []
 
-for archivo in archivos:
-    if os.path.isfile(path+archivo):
-        f = open(path+archivo)
-        line = f.readline()[:-1].split(" ")
-        num_claus.append((archivo,int(line[2])+int(line[3])))
+for filename in os.listdir(source_path):
+    assert(filename[-4:] == ".cnf")
+    lcg_filename = filename.split(".")[0] + "_lcg_edge_list"
+    LCG = conv.sat_to_LCG(source_path + "/" + filename)
+    num_claus.append((filename, len(LCG)))
 
 num_claus_ord = sorted(num_claus, key=lambda x: x[1])
 
-if not os.path.isdir(path+"utiles"):
-    os.mkdir(path+"utiles")
+if not os.path.isdir(source_path+"utiles"):
+    os.mkdir(source_path+"utiles")
 
 for f,_ in num_claus_ord[:10]:
-    shutil.copy(path+f, path+"utiles")
+    shutil.copy(source_path+f, source_path+"utiles")
