189f9d8612aa67bfb538b6e36d25b5ce9f36296f
i4vk <ivangs98@gmail.com>
2020-03-14 16:21:42 +0100

    Añade longitud de clausula más corta

--- extract_features.py
+++ extract_features.py
@@ -10,6 +10,7 @@ archivo = sys.argv[1]
 
 with open(archivo, 'r') as f:
     len_claus = 0
+    min_len = float('Inf')
     for line in f:
         div = line.split(' ')
         if div[0] != 'c':
@@ -18,6 +19,8 @@ with open(archivo, 'r') as f:
                 num_claus = int(div[3])
             else:
                 len_claus += len(div)-1
+                if (len(div)-1) < min_len:
+                    min_len = len(div)-1
 
     VIG = sat_to_VIG(archivo)
     partition = community.best_partition(VIG)
@@ -28,10 +31,11 @@ media_len_claus = len_claus/num_claus
 
 if len(sys.argv) == 3:
     if sys.argv[2] == 'csv':
-        print(f'{num_var},{num_claus},{media_len_claus},{num_communities},{modularity}')
+        print(f'{num_var},{num_claus},{media_len_claus},{min_len},{num_communities},{modularity}')
 else: 
     print(f'Num. var.: {num_var}')
     print(f'Num. claus.: {num_claus}')
     print(f"Longitud media de claus.: {media_len_claus}")
+    print(f"Long. clausula más corta: {min_len}")
     print(f'Num. communities.: {num_communities}')
     print(f'Modularity: {modularity}')
